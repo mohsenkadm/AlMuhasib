42da6007b8ebc7d9ae67d23761d751c45d8c40a0
mohsenkadm123 <mohsenkadm123@gmail.com>
2026-07-15 16:19:22 +0300

    some update

--- src/AlMuhasib.Cloud.Infrastructure/Migrations/20260713215133_SyncCloudPendingModelChanges.Designer.cs
+++ src/AlMuhasib.Cloud.Infrastructure/Migrations/20260713215133_SyncCloudPendingModelChanges.Designer.cs
@@ -4,6 +4,7 @@ using AlMuhasib.Cloud.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AlMuhasib.Cloud.Infrastructure.Migrations
 {
     [DbContext(typeof(CloudDbContext))]
-    partial class CloudDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260713215133_SyncCloudPendingModelChanges")]
+    partial class SyncCloudPendingModelChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AlMuhasib.Cloud.Infrastructure/Migrations/20260713215133_SyncCloudPendingModelChanges.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlMuhasib.Cloud.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncCloudPendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSold",
+                table: "CarTradeTransactions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SaleAmountPaid",
+                table: "CarTradeTransactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SaleDate",
+                table: "CarTradeTransactions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SalePaymentMode",
+                table: "CarTradeTransactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SaleRemainingAmount",
+                table: "CarTradeTransactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentKind",
+                table: "CarTradePayments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSold",
+                table: "CarTradeTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "SaleAmountPaid",
+                table: "CarTradeTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "SaleDate",
+                table: "CarTradeTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "SalePaymentMode",
+                table: "CarTradeTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "SaleRemainingAmount",
+                table: "CarTradeTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentKind",
+                table: "CarTradePayments");
+        }
+    }
+}
